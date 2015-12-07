177d984752bf5396b3ddf726b8daf2577de323db
aniaw <ania_w@outlook.com>
2015-12-07 12:46:34 +0100

    update scaffolding

---
+++ test/unit/app.spec.js
@@ -0,0 +1,22 @@
+(function ()
+{
+    'use strict';
+
+    var _ = require('lodash');
+    var Promise = require('bluebird');
+    var chai = require('chai');
+    var expect = chai.expect;
+    var supertestFactory = require('supertest-as-promised');
+    var uuid = require('node-uuid');
+    var authenticator = require('../../app/authenticator');
+    var app = require('../../app/app');
+    Promise.longStackTraces();
+
+    describe('app', function ()
+    {
+        it('should ...', function ()
+        {
+
+        });
+    });
+})();
