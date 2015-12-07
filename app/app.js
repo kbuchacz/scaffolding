3f616cd9433ecc8d9e85ecc10ae0f505beb9f893
aniaw <ania_w@outlook.com>
2015-12-07 12:12:38 +0100

    update scaffolding app structure

--- app/app.js
+++ app/app.js
@@ -2,7 +2,10 @@
 {
     'use strict';
 
-    angular.module('app', []);
+    angular.module('app',[]).controller('AppCtrl', function ()
+    {
+        this.message = 'Hello Angular App!';
+    });
 
 })();
 
