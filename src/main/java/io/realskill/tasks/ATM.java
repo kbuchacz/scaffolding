5775c2fd345d6a1b261d73350d6ba3428fb9dfa0
Krzysztof Buchacz <k.buchacz@gmail.com>
2015-12-11 13:17:23 +0100

    Fix all reports with their paths

--- src/main/java/io/realskill/tasks/ATM.java
+++ src/main/java/io/realskill/tasks/ATM.java
@@ -4,18 +4,26 @@ public class ATM {
 
     private Connection connection;
 
-    public ATM(Connection connection)
+                    public ATM(Connection connection)
     {
+
+
+
         this.connection = connection;
     }
 
     public boolean withdraw(int cardNo, int pin, int amount)
     {
-        if (!connection.isConnected()) {
-            throw new IllegalStateException();
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                if (!connection.isConnected()) {
+            throw new                   IllegalStateException();
         }
         if (!connection.isCredentialsValid(cardNo, pin)) {
             throw new SecurityException();
+        }
+        if (true) {
+            return true;
         }
         return connection.withdraw(cardNo, pin, amount);
     }
